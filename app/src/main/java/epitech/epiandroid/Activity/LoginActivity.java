48dc6414e3653c70004f6801be2d1b18ed3e0401
Switi <styve.simonneau@gmail.com>
2015-01-07 19:35:51 +0100

    MyRequest, regarder le Login.java pour voir comment utiliser

---
+++ app/src/main/java/epitech/epiandroid/Activity/LoginActivity.java
@@ -0,0 +1,147 @@
+package epitech.epiandroid.Activity;
+import epitech.epiandroid.MyRequest;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import epitech.epiandroid.R;
+
+public class LoginActivity extends Activity {
+
+    /**
+     * The {@link ViewPager} that will display the three primary sections of the app, one at a
+     * time.
+     */
+    private Context     sContext;
+    private LoginTask   mLoginTask;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.sContext = getApplicationContext();
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText login = (EditText) findViewById(R.id.login);
+                EditText mdp = (EditText) findViewById(R.id.mdp);
+
+                if (login.getText().toString().equals("") || mdp.getText().toString().equals(""))
+                    Toast.makeText(sContext, "Login or password is missing", Toast.LENGTH_SHORT).show();
+                else {
+                    ((TextView)(findViewById(R.id.notes))).setText("Connecting...");
+                    mLoginTask = new LoginTask(login.getText().toString(), mdp.getText().toString(), sContext.getApplicationContext());
+                    mLoginTask.execute((Void) null);
+                }
+            }
+        });
+    }
+
+    public class LoginTask extends AsyncTask<Void, Void, Boolean> {
+        private String login;
+        private String pass;
+        private Context ctx;
+        private String responseString = null;
+
+        LoginTask(String login, String password, Context ctx) {
+            this.login = login;
+            this.pass = password;
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            final String TAG = "background";
+            responseString = "";
+            Log.v(TAG, "launch background task");
+            try {
+                MyRequest.clearFields();
+                MyRequest.addField("login", login);
+                MyRequest.addField("password", pass);
+                MyRequest.CreatePost("login");
+
+                if (MyRequest.isStatusOk() || MyRequest.isStatusUnauthorized()) {
+                    responseString = MyRequest.getResponseString();
+                }
+                else if (MyRequest.isStatusTimeout()) {
+                    Toast.makeText(ctx, "Delai d'attente dépassé", Toast.LENGTH_SHORT).show();
+                }
+                else if (MyRequest.isStatusForbidden()) {
+                    mLoginTask.cancel(true);
+                    throw new IOException(MyRequest.getReasonPhrase());
+                }
+                else {
+                    responseString = MyRequest.getResponseString();
+                    Log.e(TAG, "connexion failed" + responseString);
+                    throw new IOException(MyRequest.getReasonPhrase());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+            Log.v(TAG, "index=" + responseString);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            super.onPostExecute(success);
+            TextView text = (TextView) findViewById(R.id.notes);
+            String token = "null";
+            Boolean err = true;
+            JSONObject json;
+
+            // check si le retour est bien du json
+            try {
+                json = new JSONObject(responseString);
+                // check si il y a bien un token
+                if (json.has("token") || json.has("ip")) {
+                    if (json.has("token"))
+                        token = json.getString("token");
+                    else if (json.has("ip"))
+                        token = json.getString("ip");
+                    err = false;
+                } else if (json.has("error")) {
+                    token = ((JSONObject)json.get("error")).getString("message");
+                    Toast.makeText(ctx, token, Toast.LENGTH_SHORT).show();
+                } else {
+                    token = "non handled error.";
+                }
+            } catch (JSONException e) {
+                token = "Internal server error";
+                Toast.makeText(ctx, "Internal server error", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+
+            Log.w("response", responseString);
+            text.setText(token);
+
+            // TODO : remettre les ifs pour la prod
+            if (!err) {
+                Intent i = new Intent(getApplicationContext(), SwipeActivity.class);
+                i.putExtra("token", token);
+                i.putExtra("login", login);
+                i.putExtra("pass", pass);
+                startActivity(i);
+            }
+
+            this.cancel(true);
+            mLoginTask = null;
+        }
+    }
+}
---
+++ app/src/main/java/epitech/epiandroid/MyRequest.java
@@ -0,0 +1,105 @@
+package epitech.epiandroid;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreProtocolPNames;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by Styve on 07/01/2015.
+ */
+
+public class MyRequest {
+    public static HttpPost      post;
+    public static HttpGet       get;
+    public static HttpResponse  response;
+    public static HttpClient    httpclient;
+    public static String        baseUrl;
+
+    public static int           statusCode;
+    private static MyRequest    ourInstance = new MyRequest();
+    public static MyRequest     getInstance() {
+        return ourInstance;
+    }
+    private static ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+
+    private static void InitHttpClient() {
+        try {
+            MyRequest.httpclient = new DefaultHttpClient();
+            httpclient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, System.getProperty("http.agent"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        MyRequest.baseUrl = "http://epitech-api.herokuapp.com/";
+    }
+
+    private MyRequest() {
+        MyRequest.InitHttpClient();
+    }
+
+    public static HttpResponse CreatePost(String function) {
+        MyRequest.post = new HttpPost(MyRequest.baseUrl + function);
+        MyRequest.post.setHeader("User-Agent", System.getProperty("http.agent"));
+        try {
+            MyRequest.post.setEntity(new UrlEncodedFormEntity(MyRequest.nameValuePairs));
+            MyRequest.response = httpclient.execute(MyRequest.post);
+            MyRequest.clearFields();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        MyRequest.statusCode = MyRequest.response.getStatusLine().getStatusCode();
+        return (MyRequest.response);
+    }
+
+    public static String getResponseString() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            MyRequest.response.getEntity().writeTo(out);
+            out.close();
+            try {
+                MyRequest.response.getEntity().getContent().close();
+            } catch (Exception e) {
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (out.toString());
+    }
+
+    public static String getReasonPhrase() {
+        return (MyRequest.response.getStatusLine().getReasonPhrase());
+    }
+
+    public static void addField(String field, String data) {
+        MyRequest.nameValuePairs.add(new BasicNameValuePair(field, data));
+    }
+
+    public static void clearFields() {
+        MyRequest.nameValuePairs.clear();
+    }
+
+    public static Boolean isStatusOk() {
+        return (MyRequest.statusCode == HttpStatus.SC_OK);
+    }
+
+    public static Boolean isStatusUnauthorized() {
+        return (MyRequest.statusCode == HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    public static Boolean isStatusForbidden() {
+        return (MyRequest.statusCode == HttpStatus.SC_FORBIDDEN);
+    }
+
+    public static Boolean isStatusTimeout() {
+        return (MyRequest.statusCode  == HttpStatus.SC_GATEWAY_TIMEOUT || MyRequest.statusCode == HttpStatus.SC_REQUEST_TIMEOUT);
+    }
+}
