ca0d2f05e398bd27f0d5b608415fbf9a8709eaca
LeNiglo <lefrantguillaume@gmail.com>
2015-01-10 20:10:28 +0100

    ouuupppps les fragments :/

---
+++ app/src/main/java/epitech/epiandroid/Fragment/ModulesFragment.java
@@ -0,0 +1,21 @@
+package epitech.epiandroid.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import epitech.epiandroid.R;
+
+
+public class ModulesFragment extends Fragment {
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView  = inflater.inflate(R.layout.fragment_section_token, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/epitech/epiandroid/Fragment/TrombiFragment.java
@@ -0,0 +1,29 @@
+package epitech.epiandroid.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import epitech.epiandroid.R;
+
+public class TrombiFragment extends Fragment {
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView  = inflater.inflate(R.layout.fragment_section_profil, container, false);
+
+
+
+        //TextView text = (TextView) rootView.findViewById(R.id.login);
+        //text.setText("login : " + getArguments().getString("login") + " !!");
+
+        //TextView token = (TextView) rootView.findViewById(R.id.token);
+        //token.setText(getArguments().getString("token"));
+
+        return rootView;
+    }
+}
