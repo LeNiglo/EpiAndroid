9b18acc401d36667b71e511f679cfe3a7be4d47e
LeNiglo <lefrantguillaume@gmail.com>
2015-01-20 13:46:04 +0100

    Token

--- app/src/main/java/epitech/epiandroid/Fragment/NotesFragment.java
+++ app/src/main/java/epitech/epiandroid/Fragment/NotesFragment.java
@@ -15,7 +15,7 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView  = inflater.inflate(R.layout.fragment_section_token, container, false);
+        //rootView  = inflater.inflate(android.R.layout.fragment_section_token, container, false);
         return rootView;
     }
 }
