0ca20c68dc8d467f5146595b3e016b5b0d24d4ca
Julien Genoud <juliengenoud@gmail.com>
2015-02-02 11:45:09 +0100

    menu error

--- app/src/main/java/epitech/epiandroid/Activity/DrawerActivity.java
+++ app/src/main/java/epitech/epiandroid/Activity/DrawerActivity.java
@@ -136,8 +136,17 @@ public class DrawerActivity extends MaterialNavigationDrawer<Fragment> implement
         alert.show();
     }
 
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onPrepareOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
