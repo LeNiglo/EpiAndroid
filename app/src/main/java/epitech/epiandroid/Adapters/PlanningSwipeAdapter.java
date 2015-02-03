67e43312ca4afa4f4654d2736b4c9098c206716e
LeNiglo <lefrantguillaume@gmail.com>
2015-02-03 14:07:10 +0100

    token

--- app/src/main/java/epitech/epiandroid/Adapters/PlanningSwipeAdapter.java
+++ app/src/main/java/epitech/epiandroid/Adapters/PlanningSwipeAdapter.java
@@ -8,11 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.daimajia.swipe.adapters.ArraySwipeAdapter;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -97,12 +100,12 @@ public class PlanningSwipeAdapter extends ArraySwipeAdapter<PlanningItem> {
                                         .positiveText(R.string.ok)
                                         .customView(layout, true)
                                         .callback(new MaterialDialog.ButtonCallback() {
-                                            @Override
-                                            public void onPositive(MaterialDialog dialog) {
-												// p.getScolaryear(), p.getCodemodule(), p.getCodeinstance(), p.getCodeacti(), p.getCodeevent(), v.getContext(), (Activity) v.getContext()).execute((Void) null);
-                                                Toast.makeText(v2.getContext(), "CLICKED FOR : token", Toast.LENGTH_LONG).show();
-                                            }
-                                        })
+											@Override
+											public void onPositive(MaterialDialog dialog) {
+												View view = dialog.getCustomView();
+												new TokenTask(p.getScolaryear(), p.getCodemodule(), p.getCodeinstance(), p.getCodeacti(), p.getCodeevent(), ((EditText) view.findViewById(R.id.validate_token)).getText().toString(), view.getContext(), (Activity) view.getContext()).execute((Void) null);
+											}
+										})
                                         .show();
                             }
                         });
@@ -114,7 +117,6 @@ public class PlanningSwipeAdapter extends ArraySwipeAdapter<PlanningItem> {
                             @Override
                             public void onClick(View v) {
 								new SubscribeTask(p.getScolaryear(), p.getCodemodule(), p.getCodeinstance(), p.getCodeacti(), p.getCodeevent(), v.getContext(), (Activity) v.getContext()).execute((Void) null);
-                                Toast.makeText(v.getContext(), "CLICKED FOR : register", Toast.LENGTH_LONG).show();
                             }
                         });
                     } else if (p.getRegisterStudent() && p.getEventRegistered().equals("registered")) { // on peut se désinscrire
@@ -125,7 +127,6 @@ public class PlanningSwipeAdapter extends ArraySwipeAdapter<PlanningItem> {
                             @Override
                             public void onClick(View v) {
 								new UnSubscribeTask(p.getScolaryear(), p.getCodemodule(), p.getCodeinstance(), p.getCodeacti(), p.getCodeevent(), v.getContext(), (Activity) v.getContext()).execute((Void) null);
-								Toast.makeText(v.getContext(), "CLICKED FOR : unregister", Toast.LENGTH_LONG).show();
                             }
                         });
                     } else if (p.getEventRegistered().equals("present")) { // on a été présent
