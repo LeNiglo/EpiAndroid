93c136af8a46047854cc04cc8c59f9dfaeffa5f5
LeNiglo <lefrantguillaume@gmail.com>
2015-02-03 12:31:31 +0100

    task

---
+++ app/src/main/java/epitech/epiandroid/Tasks/UnSubscribeTask.java
@@ -0,0 +1,87 @@
+package epitech.epiandroid.Tasks;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import epitech.epiandroid.Databases.LoginTable;
+import epitech.epiandroid.MyRequest;
+import epitech.epiandroid.R;
+
+/**
+ * Created by Styve on 12/01/2015.
+ */
+
+public class UnSubscribeTask extends AsyncTask<Void, Void, Boolean> {
+    private String token;
+    private String scolaryear;
+    private String codemodule;
+    private String codeinstance;
+    private String codeacti;
+	private String codeevent;
+    private Context ctx;
+    private String responseString = null;
+    private Activity activity;
+
+	public UnSubscribeTask(String scolaryear, String codemodule, String codeinstance, String codeacti, String codeevent, Context ctx, Activity activity) {
+		LoginTable infos = LoginTable.listAll(LoginTable.class).get(0);
+		this.token = infos.getToken();
+		this.scolaryear = scolaryear;
+        this.codemodule = codemodule;
+        this.codeinstance = codeinstance;
+        this.codeacti = codeacti;
+        this.codeevent = codeevent;
+        this.ctx = ctx;
+        this.activity = activity;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        responseString = "";
+        try {
+            MyRequest.clearFields();
+            MyRequest.addField("token", this.token);
+			MyRequest.addField("scolaryear", this.scolaryear);
+			MyRequest.addField("codemodule", this.codemodule);
+			MyRequest.addField("codeinstance", this.codeinstance);
+			MyRequest.addField("codeacti", this.codeacti);
+			MyRequest.addField("codeevent", this.codeevent);
+
+			MyRequest.CreatePost("project");
+
+            if (MyRequest.isStatusOk() || MyRequest.isStatusUnauthorized()) {
+                responseString = MyRequest.getResponseString();
+            }
+            else if (MyRequest.isStatusTimeout()) {
+                Toast.makeText(ctx, "Delai d'attente dépassé", Toast.LENGTH_SHORT).show();
+            }
+            else if (MyRequest.isStatusForbidden()) {
+                throw new IOException(MyRequest.getReasonPhrase());
+            }
+            else {
+                responseString = MyRequest.getResponseString();
+                throw new IOException(MyRequest.getReasonPhrase());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+        super.onPostExecute(success);
+
+		Toast.makeText(this.ctx, this.ctx.getResources().getString(R.string.unsubscribe, this.codeacti), Toast.LENGTH_LONG).show();
+
+        this.cancel(true);
+    }
+}
